b213973fd3958dbdc1946c0eb69f7acfa8405860
Rusal84 <rusal@bk.ru>
2021-02-24 03:22:23 +0300

    Console WhithGui

---
+++ ConsoleMessengerWithGUI/MessangerClientAPI.cs
@@ -0,0 +1,77 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleMessenger
+{
+  class MessangerClientAPI
+  {
+    public void TestNewtonsoftJson()
+    {
+      // Тест JSon SerializeObject NewtonSoft
+      Message msg = new Message("RusAl", "Privet", DateTime.UtcNow);
+      string output = JsonConvert.SerializeObject(msg);
+      Console.WriteLine(output);
+      Message deserializedMsg = JsonConvert.DeserializeObject<Message>(output);
+      Console.WriteLine(deserializedMsg);
+      // Create the file.
+      //string path = @"d:\temp\ser.txt";
+      //using (StreamWriter sw = new StreamWriter(path, false, System.Text.Encoding.Default))
+      //{
+      //  sw.WriteLine(output);
+      //}
+    }
+
+    public Message GetMessage(int MessageId)
+    {
+      WebRequest request = WebRequest.Create("http://localhost:5000/api/Messanger/" + MessageId.ToString());
+      request.Method = "Get";
+      WebResponse response = request.GetResponse();
+      string status = ((HttpWebResponse)response).StatusDescription;
+      //Console.WriteLine(status);
+      Stream dataStream = response.GetResponseStream();
+      StreamReader reader = new StreamReader(dataStream);
+      string responseFromServer = reader.ReadToEnd();
+      //Console.WriteLine(responseFromServer);
+      reader.Close();
+      dataStream.Close();
+      response.Close();
+      if ((status.ToLower() == "ok") && (responseFromServer != "Not found"))
+      {
+        Message deserializedMsg = JsonConvert.DeserializeObject<Message>(responseFromServer);
+        //Console.WriteLine(deserializedMsg);
+        return deserializedMsg;
+      }
+      return null;
+    }
+
+    public bool SendMessage(Message msg)
+    {
+      WebRequest request = WebRequest.Create("http://localhost:5000/api/Messanger");
+      request.Method = "POST";
+      //Message msg = new Message("RusAl", "Privet1100", DateTime.Now);
+      string postData = JsonConvert.SerializeObject(msg);
+      byte[] byteArray = Encoding.UTF8.GetBytes(postData);
+      request.ContentType = "application/json";
+      request.ContentLength = byteArray.Length;
+      Stream dataStream = request.GetRequestStream();
+      dataStream.Write(byteArray, 0, byteArray.Length);
+      dataStream.Close();
+      WebResponse response = request.GetResponse();
+      //Console.WriteLine(((HttpWebResponse)response).StatusDescription);
+      dataStream = response.GetResponseStream();
+      StreamReader reader = new StreamReader(dataStream);
+      string responseFromServer = reader.ReadToEnd();
+      //Console.WriteLine(responseFromServer);
+      reader.Close();
+      dataStream.Close();
+      response.Close();
+      return true;
+    }
+  }
+}
--- ConsoleMessengerWithGUI/Program.cs
+++ ConsoleMessengerWithGUI/Program.cs
@@ -1,10 +1,16 @@
 ﻿using System;
 using Terminal.Gui;
+using ConsoleMessenger;
+using System.Collections.Generic;
 
 namespace ConsoleMessengerWithGUI
 {
   class Program
   {
+    // Очередь сообщений
+    private static List<Message> messages = new List<Message>();
+    private static MessangerClientAPI API = new MessangerClientAPI();
+
     private static MenuBar menu;
     private static Window winMain;
     private static Window winMessages;
@@ -53,7 +59,121 @@ namespace ConsoleMessengerWithGUI
       };
       winMain.Add(winMessages);
 
+      // Текст "Пользователь"
+      labelUser = new Label()
+      {
+        X = 0,
+        Y = Pos.Bottom(winMain) - 5,
+        Width = 15,
+        Height = 1,
+        Text = "Username:",
+      };
+      winMain.Add(labelUser);
+
+      // Поле ввода имени пользователя
+      fieldUsername = new TextField()
+      {
+        X = 15,
+        Y = Pos.Bottom(winMain) - 5,
+        Width = winMain.Width - 14,
+        Height = 1,
+      };
+      winMain.Add(fieldUsername);
+
+      // Текст "Сообщение"
+      labelMessage = new Label()
+      {
+        X = 0,
+        Y = Pos.Bottom(winMain) - 4,
+        Width = 15,
+        Height = 1,
+        Text = "Message:",
+      };
+      winMain.Add(labelMessage);
+
+      // Поле ввода сообщения
+      fieldMessage = new TextField()
+      {
+        X = 15,
+        Y = Pos.Bottom(winMain) - 4,
+        Width = winMain.Width - 14,
+        Height = 1,
+      };
+      winMain.Add(fieldMessage);
+
+      // Кнопка отправки
+      btnSend = new Button()
+      {
+        X = Pos.Right(winMain) - 10 - 5,
+        Y = Pos.Bottom(winMain) - 4,
+        Width = 10,
+        Height = 1,
+        Text = "  Send  ",
+      };
+      winMain.Add(btnSend);
+
+      // Обработка клика по кнопке
+      btnSend.Clicked += OnBtnSendClick;
+
+      // Запуск цикла проверки обновлений сообщений
+      var updateLoop = new System.Timers.Timer();
+      updateLoop.Interval = 1000;
+      int MessageID = 0;
+      updateLoop.Elapsed += (sender, eventArgs) =>
+      {
+        Message msg = API.GetMessage(MessageID);
+        while (msg != null)
+        {
+          messages.Add(msg);
+          UpdateMessages();
+          MessageID++;
+          msg = API.GetMessage(MessageID);
+        }
+      };
+      updateLoop.Start();
+
       Application.Run();
+      Console.Clear();
     }
+    // При нажатии на кнопку отправки
+    static void OnBtnSendClick()
+    {
+      if (fieldMessage.Text.ToString() != "" && fieldUsername.Text.ToString() != "")
+      {
+        var msg = new Message(fieldUsername.Text.ToString(), fieldMessage.Text.ToString(), DateTime.Now);
+        API.SendMessage(msg);
+
+        // Добавление сообщений локально
+        // В этом нет необходимости, т.к. серв вернёт отправленное нами сообщение в цикле обновлений
+        // messages.Add(msg);                
+        // UpdateMessages();
+
+        fieldMessage.Text = "";
+      }
+    }
+    // Синхронизирует список сообщений с отображением
+    static void UpdateMessages()
+    {
+      // Удаляем все компоненты сообщений
+      winMessages.RemoveAll();
+      // Идём в обратном порядке и добавляе сообщения (вверху - самые новые)
+      int offset = 0;
+      for (var i = messages.Count - 1; i >= 0; i--)
+      {
+        var msg = messages[i];
+        winMessages.Add(new View()
+        {
+          X = 0,
+          Y = offset,
+          Width = winMessages.Width,
+          Height = 1,
+          Text = msg.ToString(),
+        });
+        offset++;
+      }
+      // Обвновлялм отображение
+      Application.Refresh();
+    }
+
   }
 }
